ab8c530f014c1a1fcaecd401f7a11107ac9a0cee
Paris Dolfman <parisdolfman@gmail.com>
2018-10-16 16:15:30 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -8,9 +8,9 @@ def initialize(word)
   @word = word
 end
 
- def match(word) #possibly the word variable that should be array?
-   # 1) iterate over array of word (which word variable is the array?)
-   # 2) compare each word in array to word initialized with (in original instantiation)
+ def match(word_array) #possibly the word variable that should be array?
+   word_array.each do |word_array| # 1) iterate over array of word (which word variable is the array?)
+   word_array.split("").sort == @word.split("").sort # 2) compare each word in array to word initialized with (in original instantiation)
    # 3) determine if composed of same letters by splitting word initialized into an array of letters
    # 3) cont.) using word.split("") then compare the two arrays using ==
    # 4) sort the elements in both arrays for comparison
